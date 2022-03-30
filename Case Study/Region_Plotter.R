b91835cdc8bb0cf9af7ce1bc3ad3568d36586d8d
joenomiddlename <watsonjoe8@gmail.com>
2022-03-30 08:06:44 -0700

    Add files via upload
    Uploaded code to plot the IPHC fishing stations in a nicer format, with a higher resolution BC Coastline Shapefile (coast_ALBERS).

---
+++ Case Study/Region_Plotter.R
@@ -0,0 +1,40 @@
+library(sf)
+library(rgeos)
+library(tidyverse)
+COAST <- readOGR("~/OneDrive - The University Of British Columbia/Whale Project/Data for Joe/Coast shapefile",
+layer="coast_ALBERS")
+COAST <- rgeos::gSimplify(COAST, tol=1000)
+load("~/Documents/hookCompetition/data/yelloweye_rockfish.rda")
+# Convert to sf format
+stations <- yelloweye_rockfish$set_counts
+stations <-
+  sf::st_as_sf(stations,
+               coords=c('lon','lat'),
+               crs=st_crs(4326))
+plot(stations)
+# Subset to keep only a single observation from each station (remove duplicates)
+stations <-
+  stations %>%
+  group_by(station) %>%
+  filter(row_number()==1 &
+           standard == 'Y' &
+           year > 1996)
+
+stations <-
+  sf::st_transform(stations,crs=COAST@proj4string)
+
+COAST_sf <- st_as_sf(COAST)
+
+stations %>%
+  ggplot() +
+  geom_sf(colour='yellow') +
+  ylim(c(range(st_coordinates(stations)[,2]))) +
+  xlim(c(range(st_coordinates(stations)[,1]))) +
+  #theme(panel.background = element_blank()) +
+  theme(panel.background = element_rect(fill='light blue')) +
+  geom_sf(data=COAST_sf, fill='light green', alpha=0.8) +
+  ggtitle('The locations of all 174 IPHC fishing stations') +
+  xlab('Longitude') + ylab('Latitude')
+
+
+
